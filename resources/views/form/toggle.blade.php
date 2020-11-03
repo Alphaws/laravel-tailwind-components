ed3c27a45973f90bb0d4f410ebbe596120734d04
Imre Szeness <imre@prstart.co.uk>
2020-11-03 03:16:50 +0100

    job title

---
+++ resources/views/form/toggle.blade.php
@@ -0,0 +1,28 @@
+<div class="flex items-center justify-center w-full mb-24">
+    <!-- Toggle Button -->
+    <label
+        for="toogleName"
+        class="flex items-center cursor-pointer"
+    >
+        <!-- toggle -->
+        <div class="relative">
+            <!-- input -->
+            <input id="toogleName" type="checkbox" class="hidden" />
+            <!-- line -->
+            <div
+                class="toggle__line w-10 h-4 bg-gray-300 rounded-full shadow-inner"
+            ></div>
+            <!-- dot -->
+            <div
+                class="toggle__dot absolute w-6 h-6 bg-red-600 rounded-full shadow inset-y-0 left-0"
+            ></div>
+        </div>
+        <!-- label -->
+        <div
+            class="ml-3 text-gray-700 font-medium"
+        >
+            Toggle Me!
+        </div>
+    </label>
+
+</div>
